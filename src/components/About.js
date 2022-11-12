20cc74ea8eb4fb6972a294ee5379d82665e616d7
Gods-Blessing <mohitadhana2569@gmail.com>
2022-11-12 12:04:52 +0530

    about page updated

--- src/components/About.js
+++ src/components/About.js
@@ -4,6 +4,9 @@ export default function About() {
   return (
     <>
      <h1>About the TextUtils</h1> 
+     <div className='container'>
+        <p>TextUtils is simply a set of utility functions to do operations on String objects</p>
+     </div>
     </>
   )
 }
